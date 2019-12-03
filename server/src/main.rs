d216314f312669e6cc5562acd483cc2e8a0a1398
Matt Bell <mappum@gmail.com>
2019-12-02 16:39:57 -0800

    Added Tendermint RPC polling

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,24 +4,36 @@ use bitcoincore_rpc::{
     json as btc_json,
     RpcApi
 };
+use tendermint::rpc as tm_rpc;
+use serde::{Deserialize, Serialize};
 
 fn main() {
-    let socket = ws::WebSocket::new(NullFactory).unwrap();
-    let broadcast = socket.broadcaster();
+    let websocket = ws::WebSocket::new(NullFactory).unwrap();
+    let broadcast = websocket.broadcaster();
 
-    let btc = bitcoin_rpc(18443).unwrap();
+    let btc = bitcoin_rpc("localhost:18443").unwrap();
     let mut btc_height = 0;
-    std::thread::spawn(move || loop {
-        let blocks = get_new_btc_blocks(&btc, btc_height).unwrap();
-        blocks.last().map(|b| btc_height = b.height as u64);
 
-        let message = format!("{:?}", blocks);
-        
+    let tm = tendermint_rpc("kep.io:26657").unwrap();
+    let mut tm_height = 0;
+
+    std::thread::spawn(move || loop {
+        let btc_blocks = get_new_btc_blocks(&btc, btc_height).unwrap();
+        btc_blocks.last().map(|b| btc_height = b.height as u64);
+
+        let message = serde_json::to_string(&btc_blocks).unwrap();
         broadcast.send(message).unwrap();
-        std::thread::sleep_ms(1000);
+
+        let tm_blocks = get_new_tm_blocks(&tm, tm_height).unwrap();
+        tm_blocks.last().map(|b| tm_height = b.height.value());
+
+        let message = serde_json::to_string(&tm_blocks).unwrap();
+        broadcast.send(message).unwrap();
+
+        std::thread::sleep_ms(500);
     });
 
-    socket.listen("localhost:8080").unwrap();
+    websocket.listen("localhost:8080").unwrap();
 }
 
 struct NullHandler;
@@ -31,12 +43,12 @@ struct NullFactory;
 impl ws::Factory for NullFactory {
     type Handler = NullHandler;
     
-    fn connection_made(&mut self, conn: ws::Sender) -> NullHandler {
+    fn connection_made(&mut self, _conn: ws::Sender) -> NullHandler {
         NullHandler
     }
 }
 
-fn bitcoin_rpc(port: u16) -> btc_rpc::Result<btc_rpc::Client> {
+fn bitcoin_rpc(address: &str) -> btc_rpc::Result<btc_rpc::Client> {
     use btc_rpc::{Auth, Client};
 
     let user = std::env::var("BTC_RPC_USER")
@@ -44,7 +56,7 @@ fn bitcoin_rpc(port: u16) -> btc_rpc::Result<btc_rpc::Client> {
     let pass = std::env::var("BTC_RPC_PASS")
         .expect("expected env var 'BTC_RPC_PASS'");
     let auth = Auth::UserPass(user, pass);
-    Client::new(format!("http://localhost:{}", port), auth)
+    Client::new(format!("http://{}", address), auth)
 }
 
 fn get_new_btc_blocks(
@@ -63,6 +75,33 @@ fn get_new_btc_blocks(
         let hash = rpc.get_block_hash(h)?;
         let block = rpc.get_block_info(&hash)?;
         blocks.push(block);
+    }
+
+    Ok(blocks)
+}
+
+fn tendermint_rpc(
+    address: &str
+) -> Result<tm_rpc::Client, tm_rpc::Error> {
+    let address = address.parse().expect("invalid address");
+    tm_rpc::Client::new(&address)
+}
+
+fn get_new_tm_blocks(
+    rpc: &tm_rpc::Client,
+    current_height: u64
+) -> Result<Vec<tendermint::block::Header>, tm_rpc::Error> {
+    let height = rpc.status()?.sync_info.latest_block_height.value();
+
+    let mut blocks = vec![];
+    let start_height = max(
+        current_height,
+        max(height, 5) - 5
+    ) + 1;
+
+    for h in start_height..=height {
+        let res = rpc.block(h)?;
+        blocks.push(res.block.header);
     }
 
     Ok(blocks)
