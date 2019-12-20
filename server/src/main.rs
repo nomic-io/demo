465688f56cae8dbe67366e2aa6c366eff708b8e1
Matt Bell <mappum@gmail.com>
2019-12-19 18:46:12 -0800

    Added PORT env var to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,18 +1,14 @@
-use std::cmp::{min, max};
+use bitcoin::blockdata::block::BlockHeader;
+use bitcoin::util::hash::BitcoinHash;
+use bitcoincore_rpc::{self as btc_rpc, json as btc_json, RpcApi};
+use failure::{bail, Error};
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+use std::cmp::{max, min};
 use std::collections::VecDeque;
 use std::io::Write;
 use std::sync::{Arc, Mutex};
-use bitcoincore_rpc::{
-    self as btc_rpc,
-    json as btc_json,
-    RpcApi
-};
 use tendermint::rpc as tm_rpc;
-use serde::{Deserialize, Serialize};
-use failure::{bail, Error};
-use sha2::{Digest, Sha256};
-use bitcoin::blockdata::block::BlockHeader;
-use bitcoin::util::hash::BitcoinHash;
 
 type Result<T> = std::result::Result<T, Error>;
 
@@ -25,7 +21,8 @@ fn main() {
 
     let factory = WSFactory(
         server.bitcoin_blocks.clone(),
-        server.tendermint_blocks.clone()
+        server.tendermint_blocks.clone(),
+        server.validators.clone()
     );
 
     let websocket = ws::Builder::new()
@@ -35,26 +32,33 @@ fn main() {
     let broadcaster = websocket.broadcaster();
 
     server.start(broadcaster);
-    
-    websocket.listen("0.0.0.0:8080").unwrap();
+
+    websocket.listen(
+        format!(
+            "0.0.0.0:{}",
+            std::env::var("PORT").unwrap_or("8080")
+        )
+    ).unwrap();
 }
 
 struct NullHandler;
 impl ws::Handler for NullHandler {}
 
-struct WSFactory (
+struct WSFactory(
     Arc<Mutex<VecDeque<BtcBlock>>>,
-    Arc<Mutex<VecDeque<TmBlock>>>
+    Arc<Mutex<VecDeque<TmBlock>>>,
+    Arc<Mutex<Vec<tendermint::validator::Info>>>
 );
 impl ws::Factory for WSFactory {
     type Handler = NullHandler;
-    
+
     fn connection_made(&mut self, conn: ws::Sender) -> NullHandler {
         eprintln!("incoming connection");
 
         let update = Update {
             bitcoin: self.0.lock().unwrap().clone().into_iter().collect(),
-            tendermint: self.1.lock().unwrap().clone().into_iter().collect()
+            tendermint: self.1.lock().unwrap().clone().into_iter().collect(),
+            validators: self.2.lock().unwrap().clone()
         };
         let message = serde_json::to_string(&update).unwrap();
         conn.send(format!("{}\n", message)).unwrap();
@@ -66,10 +70,7 @@ impl ws::Factory for WSFactory {
 #[derive(Serialize, Clone)]
 enum TmTx {
     HeaderRelay(Vec<String>),
-    WorkProof {
-        value: u64,
-        pubkey: String
-    }
+    WorkProof { value: u64, pubkey: String },
 }
 
 impl From<tendermint::abci::transaction::Transaction> for TmTx {
@@ -110,7 +111,9 @@ impl From<tendermint::abci::transaction::Transaction> for TmTx {
             let mut leading_zeros = 0;
             for byte in hash {
                 leading_zeros += byte.leading_zeros();
-                if byte.leading_zeros() != 8 { break }
+                if byte.leading_zeros() != 8 {
+                    break;
+                }
             }
 
             let mut pubkey = String::new();
@@ -121,7 +124,7 @@ impl From<tendermint::abci::transaction::Transaction> for TmTx {
 
             TmTx::WorkProof {
                 value: 1 << leading_zeros,
-                pubkey
+                pubkey,
             }
         } else {
             println!("{:?}", json);
@@ -135,12 +138,16 @@ struct TmBlock {
     height: u64,
     hash: tendermint::hash::Hash,
     time: tendermint::time::Time,
-    txs: Vec<TmTx>
+    txs: Vec<TmTx>,
 }
 
 impl From<tm_rpc::endpoint::block::Response> for TmBlock {
     fn from(block: tm_rpc::endpoint::block::Response) -> Self {
-        let txs = block.block.data.into_vec().into_iter()
+        let txs = block
+            .block
+            .data
+            .into_vec()
+            .into_iter()
             .map(|tx| tx.into())
             .collect();
 
@@ -148,7 +155,7 @@ impl From<tm_rpc::endpoint::block::Response> for TmBlock {
             height: block.block.header.height.value(),
             hash: block.block_meta.block_id.hash,
             time: block.block.header.time,
-            txs
+            txs,
         }
     }
 }
@@ -158,7 +165,7 @@ struct BtcBlock {
     height: u64,
     hash: String,
     num_txs: u32,
-    time: u32
+    time: u32,
 }
 
 impl From<btc_json::GetBlockResult> for BtcBlock {
@@ -167,7 +174,7 @@ impl From<btc_json::GetBlockResult> for BtcBlock {
             height: block.height as u64,
             hash: block.hash.to_string(),
             num_txs: block.n_tx as u32,
-            time: block.time as u32
+            time: block.time as u32,
         }
     }
 }
@@ -175,14 +182,16 @@ impl From<btc_json::GetBlockResult> for BtcBlock {
 #[derive(Serialize)]
 struct Update {
     bitcoin: Vec<BtcBlock>,
-    tendermint: Vec<TmBlock>
+    tendermint: Vec<TmBlock>,
+    validators: Vec<tendermint::validator::Info>
 }
 
 struct Server {
     bitcoin_rpc: btc_rpc::Client,
     tendermint_rpc: tm_rpc::Client,
     bitcoin_blocks: Arc<Mutex<VecDeque<BtcBlock>>>,
-    tendermint_blocks: Arc<Mutex<VecDeque<TmBlock>>>
+    tendermint_blocks: Arc<Mutex<VecDeque<TmBlock>>>,
+    validators: Arc<Mutex<Vec<tendermint::validator::Info>>>
 }
 
 impl Server {
@@ -194,7 +203,8 @@ impl Server {
             bitcoin_rpc,
             tendermint_rpc,
             bitcoin_blocks: Default::default(),
-            tendermint_blocks: Default::default()
+            tendermint_blocks: Default::default(),
+            validators: Default::default()
         })
     }
 
@@ -210,9 +220,12 @@ impl Server {
                 let tm_blocks = self.get_new_tm_blocks(5).unwrap();
 
                 if !btc_blocks.is_empty() || !tm_blocks.is_empty() {
+                    let validators = self.get_tm_validators().unwrap();
+
                     let update = Update {
                         bitcoin: btc_blocks,
-                        tendermint: tm_blocks
+                        tendermint: tm_blocks,
+                        validators
                     };
                     let message = serde_json::to_string(&update).unwrap();
                     broadcaster.send(format!("{}\n", message)).unwrap();
@@ -227,20 +240,15 @@ impl Server {
         let bitcoin_blocks = self.bitcoin_blocks.lock().unwrap();
         match bitcoin_blocks.back() {
             None => 0,
-            Some(block) => block.height
+            Some(block) => block.height,
         }
     }
 
-    fn get_new_btc_blocks(
-        &mut self
-    ) -> Result<Vec<BtcBlock>> {
+    fn get_new_btc_blocks(&mut self) -> Result<Vec<BtcBlock>> {
         let height = self.bitcoin_rpc.get_block_count()?;
 
         let mut blocks = vec![];
-        let start_height = max(
-            self.btc_height(),
-            max(height, 5) - 5
-        ) + 1;
+        let start_height = max(self.btc_height(), max(height, 5) - 5) + 1;
 
         let mut bitcoin_blocks = self.bitcoin_blocks.lock().unwrap();
 
@@ -262,19 +270,20 @@ impl Server {
         let tendermint_blocks = self.tendermint_blocks.lock().unwrap();
         match tendermint_blocks.back() {
             None => 0,
-            Some(block) => block.height
+            Some(block) => block.height,
         }
     }
 
     fn get_new_tm_blocks(&mut self, max_scan: u64) -> Result<Vec<TmBlock>> {
-        let height = self.tendermint_rpc.status()?
-            .sync_info.latest_block_height.value();
-        
+        let height = self
+            .tendermint_rpc
+            .status()?
+            .sync_info
+            .latest_block_height
+            .value();
+
         let mut blocks = vec![];
-        let start_height = max(
-            self.tm_height(),
-            max(height, max_scan) - max_scan
-        ) + 1;
+        let start_height = max(self.tm_height(), max(height, max_scan) - max_scan) + 1;
 
         let mut tendermint_blocks = self.tendermint_blocks.lock().unwrap();
 
@@ -300,15 +309,21 @@ impl Server {
 
         Ok(blocks)
     }
+
+    fn get_tm_validators(&self) -> Result<Vec<tendermint::validator::Info>> {
+        let height = self.tm_height();
+        let req = tm_rpc::endpoint::validators::Request::new(height.into());
+        let validators = self.tendermint_rpc.perform(req)?.validators;
+        *self.validators.lock().unwrap() = validators.clone();
+        Ok(validators)
+    }
 }
 
 fn connect_btc_rpc(address: &str) -> btc_rpc::Result<btc_rpc::Client> {
     use btc_rpc::{Auth, Client};
 
-    let user = std::env::var("BTC_RPC_USER")
-        .expect("expected env var 'BTC_RPC_USER'");
-    let pass = std::env::var("BTC_RPC_PASS")
-        .expect("expected env var 'BTC_RPC_PASS'");
+    let user = std::env::var("BTC_RPC_USER").expect("expected env var 'BTC_RPC_USER'");
+    let pass = std::env::var("BTC_RPC_PASS").expect("expected env var 'BTC_RPC_PASS'");
     let auth = Auth::UserPass(user, pass);
     Client::new(format!("http://{}", address), auth)
 }
